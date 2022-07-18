80b8b8a5797034861201c52d8835cb61fd34d769
tiennsangg <dotiensang123@gmail.com>
2022-07-18 21:34:37 +0700

    done 99%

--- assets/js/flickity.js
+++ assets/js/flickity.js
@@ -9,10 +9,44 @@ function slide(){
         "lazyLoad": 1,
         "draggable": true,
         "prevNextButtons": true,
-        "pageDots": true,
+        "pageDots": false,
         "groupCells": true, //quấn đến cuối sẽ ko ấn đc nữa, nếu ko dùng vẫn ấn đc 2-3 lần mới hết
-        "wrapAround":true, //Quấn quanh không dừng lại
+        "wrapAround":false, //Quấn quanh không dừng lại
       });
 }
 slide();
-//slick
+
+
+function slide2(){
+  $('.s_container').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true,
+      "freeScroll": false,// ảnh dừng sẽ ko bị đứt
+      "imagesLoaded": true, //cần dùng khi dùng freescroll
+      "lazyLoad": 1,
+      "draggable": true,
+      "prevNextButtons": true,
+      "pageDots": false,
+      "groupCells": true, //quấn đến cuối sẽ ko ấn đc nữa, nếu ko dùng vẫn ấn đc 2-3 lần mới hết
+      "wrapAround":false, //Quấn quanh không dừng lại
+    });
+}
+slide2();
+
+function slide_ulike(){
+  $('.product_u_like').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true,
+      "freeScroll": false,// ảnh dừng sẽ ko bị đứt
+      "imagesLoaded": true, //cần dùng khi dùng freescroll
+      "lazyLoad": 1,
+      "draggable": true,
+      "prevNextButtons": true,
+      "pageDots": false,
+      "groupCells": true, //quấn đến cuối sẽ ko ấn đc nữa, nếu ko dùng vẫn ấn đc 2-3 lần mới hết
+      "wrapAround":false, //Quấn quanh không dừng lại
+    });
+}
+slide_ulike();

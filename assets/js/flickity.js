bd6757b060040320767e179cd7e50c7b62dd5bf6
tiennsangg <dotiensang123@gmail.com>
2022-07-19 15:10:04 +0700

    done 99%

--- assets/js/flickity.js
+++ assets/js/flickity.js
@@ -50,3 +50,71 @@ function slide_ulike(){
     });
 }
 slide_ulike();
+
+function slide_index_1(){
+  $('.new_product_slide').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true,
+      "freeScroll": false,// ảnh dừng sẽ ko bị đứt
+      "imagesLoaded": true, //cần dùng khi dùng freescroll
+      "lazyLoad": 1,
+      "draggable": true,
+      "prevNextButtons": true,
+      "pageDots": false,
+      "groupCells": true, //quấn đến cuối sẽ ko ấn đc nữa, nếu ko dùng vẫn ấn đc 2-3 lần mới hết
+      "wrapAround":false, //Quấn quanh không dừng lại
+    });
+}
+slide_index_1();
+
+function slide_index_2(){
+  $('.slide_content').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true,
+      "freeScroll": false,// ảnh dừng sẽ ko bị đứt
+      "imagesLoaded": true, //cần dùng khi dùng freescroll
+      "lazyLoad": 1,
+      "draggable": true,
+      "prevNextButtons": true,
+      "pageDots": false,
+      "groupCells": true, //quấn đến cuối sẽ ko ấn đc nữa, nếu ko dùng vẫn ấn đc 2-3 lần mới hết
+      "wrapAround":false, //Quấn quanh không dừng lại
+    });
+}
+slide_index_2();
+
+function slide_index_3(){
+  $('.category_slide').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true,
+      "freeScroll": false,// ảnh dừng sẽ ko bị đứt
+      "imagesLoaded": true, //cần dùng khi dùng freescroll
+      "lazyLoad": 1,
+      "draggable": true,
+      "prevNextButtons": true,
+      "pageDots": false,
+      "groupCells": true, //quấn đến cuối sẽ ko ấn đc nữa, nếu ko dùng vẫn ấn đc 2-3 lần mới hết
+      "wrapAround":false, //Quấn quanh không dừng lại
+    });
+}
+slide_index_3();
+
+function slide_index_4(){
+  $('.feedback_slide').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true,
+      "freeScroll": false,// ảnh dừng sẽ ko bị đứt
+      "imagesLoaded": true, //cần dùng khi dùng freescroll
+      "lazyLoad": 1,
+      "draggable": true,
+      "prevNextButtons": true,
+      "pageDots": false,
+      "groupCells": true, //quấn đến cuối sẽ ko ấn đc nữa, nếu ko dùng vẫn ấn đc 2-3 lần mới hết
+      "wrapAround":false, //Quấn quanh không dừng lại
+    });
+}
+slide_index_4();
